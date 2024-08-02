bf6efdda73d007acd4a6377393058f9da3e150cb
Afdholanabil <afdholanabil41435@gmail.com>
2024-08-02 15:19:08 +0700

    adding dependencies and font

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,13 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
 }
 
 dependencies {
@@ -44,4 +50,28 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    val cameraxVersion = "1.2.3"
+    implementation("androidx.camera:camera-camera2:$cameraxVersion")
+    implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
+    implementation("androidx.camera:camera-view:$cameraxVersion")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+
+    implementation("androidx.exifinterface:exifinterface:1.3.7")
+
+    implementation("androidx.activity:activity-ktx:1.9.1")
+    implementation("androidx.fragment:fragment-ktx:1.8.2")
+
+    implementation("androidx.lifecycle:lifecycle-view-model-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.4")
+
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    implementation ("androidx.paging:paging-runtime-ktx:3.3.1")
+
 }
