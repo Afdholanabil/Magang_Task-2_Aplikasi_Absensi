4e88292d03202e495e6e916ddc7fc42f29bd88f7
Afdholanabil <afdholanabil41435@gmail.com>
2024-08-02 15:27:59 +0700

    made a clean folder structure

--- app/src/main/java/com/example/task2_attendright/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/task2_attendright/presentation/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.task2_attendright
+package com.example.task2_attendright.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.task2_attendright.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
